773344642b4ff02fb0d4308bdc243344e96d0faa
Wendyyy21 <wendykhong916@gmail.com>
2025-09-19 21:39:29 +0800

    included points earned in alert on payments page

--- includes/gopay.php
+++ includes/gopay.php
@@ -12,13 +12,21 @@ if(!isset($_SESSION['points'])) {
     $_SESSION['points'] = 0;
 }
 
+$points_earned = 0;
+
 if($_SERVER['REQUEST_METHOD'] === "POST") {
     $amount = $_POST['amount'] ?? '';
     $merchant = $_POST['merchant'] ?? '';
     $affiliated_merchants = ["Starbucks", "Shopee", "Grab"];
     if(in_array($merchant, $affiliated_merchants)){
-        $_SESSION['points'] += floor($amount * $merchant_multipliers[$merchant]);
+        $points_earned = floor($amount * $merchant_multipliers[$merchant]);
+        $_SESSION['points'] += $points_earned;
     } 
+
+    echo json_encode([
+        "success" => true,
+        "points_earned" => $points_earned,
+    ]);
+    exit;
 }
-header("Location: ../gopay.html");
 ?>
